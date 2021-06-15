48062647bb545b121f6660cc6b58d28c722fd06d
Pyros <zack.c.dell@gmail.com>
2021-06-14 23:47:07 -0700

    Initial commit for infra

---
+++ src/AssetInterface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+
+struct AssetType
+{
+
+};
+
+struct AssetRef
+{
+
+};
---
+++ src/AssetManager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "AssetInterface.h"
+#include <unordered_map>
+
+
+struct Asset
+{
+
+};
+
+
+class AssetManager
+{
+public:
+
+private:
+
+};
